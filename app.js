9615b5227be1ac76e1de5bc6209689b78cdbd8d0
aaronkimbrooktec <218adk@gmail.com>
2023-11-19 13:49:49 -0500

    update gitignore

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import CourseRoutes from './courses/routes.js';
 import ModuleRoutes from './modules/routes.js';
 import Hello from "./hello.js"
 import cors from 'cors'
+import "dotenv/config";
+import session from 'express-session';
 const app = express()
 app.use(cors())
 app.use(express.json());
