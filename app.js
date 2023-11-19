c436ddbe9af8b644340a9b4db142accc99ad2d6f
aaronkimbrooktec <218adk@gmail.com>
2023-11-19 13:46:07 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import Lab5 from "./lab5.js";
+import CourseRoutes from './courses/routes.js';
+import ModuleRoutes from './modules/routes.js';
+import Hello from "./hello.js"
+import cors from 'cors'
+const app = express()
+app.use(cors())
+app.use(express.json());
+Hello(app)
+Lab5(app);
+CourseRoutes(app);
+ModuleRoutes(app);
+app.listen(process.env.PORT || 4000);
