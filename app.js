0377667aea75a5fe2c59cf96886fcd706e78aa3e
aaronkimbrooktec <218adk@gmail.com>
2023-12-04 14:54:19 -0500

    add db connections string

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ import session from 'express-session';
 import mongoose from "mongoose";
 import UserRoutes from "./users/routes.js";
 
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+console.log(DB_CONNECTION_STRING)
+mongoose.connect(DB_CONNECTION_STRING);
 const app = express()
 app.use(
     cors({
       credentials: true,
-      origin: "http://localhost:3000",
+      origin: process.env.FRONTEND_URL,
     })
    );   
    const sessionOptions = {
@@ -22,6 +24,13 @@ app.use(
     resave: false,
     saveUninitialized: false,
   };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }  
   app.use(
     session(sessionOptions)
   );
