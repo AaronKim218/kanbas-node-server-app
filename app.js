57d9a691c01f3660a014eb56d4ccc627a60391d2
aaronkimbrooktec <218adk@gmail.com>
2023-12-04 14:35:51 -0500

    a6 working

--- app.js
+++ app.js
@@ -6,11 +6,30 @@ import Hello from "./hello.js"
 import cors from 'cors'
 import "dotenv/config";
 import session from 'express-session';
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express()
-app.use(cors())
+app.use(
+    cors({
+      credentials: true,
+      origin: "http://localhost:3000",
+    })
+   );   
+   const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+  };
+  app.use(
+    session(sessionOptions)
+  );
+
 app.use(express.json());
 Hello(app)
 Lab5(app);
 CourseRoutes(app);
 ModuleRoutes(app);
+UserRoutes(app);
 app.listen(process.env.PORT || 4000);
